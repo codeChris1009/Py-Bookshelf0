3809ed686d0304779b91bc4c85abedc477942185
Alan Su <susx@AlandeMacBook-Air.local>
2025-08-12 00:20:00 +0800

    [Add] FastApi with CORS

--- backend/main.py
+++ backend/main.py
@@ -4,4 +4,30 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List
 
+class Book(BaseModel):
+    isbn: str
+    language: str
+    title: str
+    author: str
+    category: str
+    year: int
+    ordinalVolume: int
+    isRead: bool
+    star: int
+
+class Bookshelf(BaseModel):
+    books: List[Book]
+
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000"
+    ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
